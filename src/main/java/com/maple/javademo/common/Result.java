98d4be4279d575fe1902bfea98aa1d161a69c1a4
753517165@qq.com <753517165@qq.com>
2017-07-04 23:35:06 +0800

    update

--- src/main/java/com/maple/javademo/common/Result.java
+++ src/main/java/com/maple/javademo/common/Result.java
@@ -6,4 +6,42 @@ package com.maple.javademo.common;
  * Created time on 2017/7/4 下午11:21.
  */
 public class Result {
+    private int code;
+    private String msg;
+    private Object model;
+
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getModel() {
+        return model;
+    }
+
+    public void setModel(Object model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", model=" + model +
+                '}';
+    }
+
 }
