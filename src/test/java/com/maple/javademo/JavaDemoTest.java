42d0a66db2b43fd3991c0cfa562f8358fb091c4e
753517165@qq.com <753517165@qq.com>
2017-07-22 15:53:37 +0800

    update

---
+++ src/test/java/com/maple/javademo/JavaDemoTest.java
@@ -0,0 +1,23 @@
+package com.maple.javademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author maple
+ * @name 金色木叶枫
+ * Created time on 2017/7/22 下午3:51.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaDemoTest {
+
+    @Test
+    public void getName(){
+
+        System.out.println("1212");
+
+    }
+}
