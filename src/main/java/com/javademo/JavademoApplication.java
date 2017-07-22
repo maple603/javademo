5536d4befdf5fd26080a914b38f5a695c5c24163
maple <jinsemuyefeng@gmail.com>
2017-07-22 23:44:05 +0800

    change project

--- src/main/java/com/javademo/JavademoApplication.java
+++ src/main/java/com/javademo/JavademoApplication.java
@@ -1,11 +1,7 @@
-package com.javademo.start;
+package com.javademo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 
 //@ComponentScan(basePackages = {"com.javademo.demo","com.javademo.start"})
 @SpringBootApplication
