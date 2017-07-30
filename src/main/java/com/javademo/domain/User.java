3237396d0c5a63e5bfd32bd8cb3c2e414001bb0d
maple <jinsemuyefeng@gmail.com>
2017-07-30 14:05:49 +0800

    update

---
+++ src/main/java/com/javademo/domain/User.java
@@ -0,0 +1,17 @@
+package com.javademo.domain;
+
+import lombok.Data;
+
+/**
+ * @author maple
+ * @name 金色木叶枫
+ * @since Created time on 2017/7/30 下午1:59.
+ */
+@Data
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+}
---
+++ src/main/java/com/javademo/web/UserController.java
@@ -0,0 +1,57 @@
+package com.javademo.web;
+
+import com.javademo.domain.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+/**
+ * @author maple
+ * @name 金色木叶枫
+ * @since Created time on 2017/7/30 下午1:58.
+ */
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
+
+
+    @RequestMapping(value = "/",method= RequestMethod.GET)
+    public List<User> getUserList(){
+        List<User> list = new ArrayList<User>(users.values());
+        return list;
+    }
+
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public String postUser(@ModelAttribute User user) {
+        // 处理"/users/"的POST请求，用来创建User
+        // 除了@ModelAttribute绑定参数之外，还可以通过@RequestParam从页面中传递参数
+        users.put(user.getId(), user);
+        return "success";
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        // 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
+        // url中的id可通过@PathVariable绑定到函数的参数中
+        return users.get(id);
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public String putUser(@PathVariable Long id, @ModelAttribute User user) {
+        // 处理"/users/{id}"的PUT请求，用来更新User信息
+        User u = users.get(id);
+        u.setName(user.getName());
+        u.setAge(user.getAge());
+        users.put(id, u);
+        return "success";
+    }
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+        // 处理"/users/{id}"的DELETE请求，用来删除User
+        users.remove(id);
+        return "success";
+    }
+
+}
