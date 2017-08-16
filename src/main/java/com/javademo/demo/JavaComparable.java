317287c0bafb88213d54d7b4ddff4d76d8fb6ecf
maple <jinsemuyefeng@gmail.com>
2017-08-17 00:11:08 +0800

    update

--- src/main/java/com/javademo/demo/JavaComparable.java
+++ src/main/java/com/javademo/demo/JavaComparable.java
@@ -2,6 +2,10 @@ package com.javademo.demo;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author maple
  * @name 金色木叶枫
@@ -10,10 +14,39 @@ import lombok.Data;
 @Data
 public class JavaComparable implements Comparable<JavaComparable> {
     private String name;
-    private Integer age;
+    private int age;
+
+    public JavaComparable(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
 
     @Override
     public int compareTo(JavaComparable object) {
-        return this.age-object.age;
+        return this.getAge()-object.getAge();
+    }
+
+    @Override
+    public String toString() {
+        return "JavaComparable{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args){
+        List<JavaComparable> arrayList = new ArrayList<>();
+        arrayList.add(new JavaComparable("dd",20));
+        arrayList.add(new JavaComparable("dd2",10));
+        arrayList.add(new JavaComparable("dd3",40));
+        arrayList.add(new JavaComparable("dd4",50));
+
+        Collections.sort(arrayList);
+
+        for (JavaComparable javaComparable : arrayList){
+            System.out.println(javaComparable.toString());
+        }
+
+
     }
 }
