2fd2d48d170d28b5cb10ec6eaf4e41b505a67e54
maple <jinsemuyefeng@gmail.com>
2017-08-27 19:16:05 +0800

    增加scheduled

--- src/main/java/com/javademo/demo/ScheduledTasks.java
+++ src/main/java/com/javademo/demo/ScheduledTasks.java
@@ -18,5 +18,14 @@ public class ScheduledTasks {
     public void reportCurrentTime() {
         System.out.println("现在时间：" + dateFormat.format(new Date()));
     }
-
+    //上一次执行完毕时间点之后5秒再执行
+    @Scheduled(fixedDelay = 5000)
+    public void outcontent(){
+        System.out.println("现在时间-1：" + dateFormat.format(new Date()));
+    }
+    //第一次延迟1秒后执行，之后按fixedRate的规则每5秒执行一次
+    @Scheduled(initialDelay = 1000,fixedRate = 5000)
+    public  void outLine(){
+        System.out.println("现在时间-2：" + dateFormat.format(new Date()));
+    }
 }
